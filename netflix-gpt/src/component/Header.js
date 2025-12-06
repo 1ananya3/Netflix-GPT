b5866f4fac7bd430d160af0b79c16ed7f9dbc803
Ananya <94430989+1ananya3@users.noreply.github.com>
2025-12-06 06:24:02 +0000

    unsubscribe when component unmounts

--- netflix-gpt/src/component/Header.js
+++ netflix-gpt/src/component/Header.js
@@ -15,19 +15,21 @@ const Header = () => {
     });
 
   }
-       useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                const { uid, email, displayName, photoURL } = user;
-                dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }))
-                navigate('/browse')
-            } else {
-                dispatch(removeUser())
-                navigate("/")
-            }
-        });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }))
+        navigate('/browse')
+      } else {
+        dispatch(removeUser())
+        navigate("/")
+      }
+    });
+    // Unsubscribe when component unmounts
+    return ()=> unsubscribe();
 
-    }, [])
+  }, [])
   return (
     <div className='absolute px-8 py-2 w-full bg-gradient-to-b from-black z-10 flex justify-between'>
       <img className='w-44' src='https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt="logo" />
